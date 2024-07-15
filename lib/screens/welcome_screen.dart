9361a0b0ddf4a7990d91fb6ef9f9badf4f366169
Tomas Rodriguez <tomas_rodriguez@epam.com>
2024-07-15 17:02:11 +0200

    added new feel and look to welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,36 +3,51 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Image.asset('assets/images/logo.png', fit: BoxFit.contain),
+        title: Text(
+          'EPAM Cocktail',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.background,
+            fontFamily: 'EPAM2',
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+          ),
+        ),
       ),
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(25),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              
+            children: [
               Text(
                 'Welcome to EPAM Cocktail!',
                 style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.primary,
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontFamily: 'EPAM2',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20,
                 ),
               ),
-              SizedBox(height: 50),
+              Image.asset('assets/images/logo.png'),
               ElevatedButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/recipes');
                 },
-                child: const Text('Go to Recipes'),
+                child: Text(
+                  'Go to Cocktails',
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontFamily: 'EPAM2',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                ),
               ),
             ],
           ),
