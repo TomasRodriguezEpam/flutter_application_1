953b8b04779ef754b9b6e1970e709f1740ccab8f
Tomas Rodriguez <tomas_rodriguez@epam.com>
2024-07-16 11:24:10 +0200

    remove unneccesary imports

--- lib/screens/add_recipe_screen.dart
+++ lib/screens/add_recipe_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/models/models.dart';
-import 'package:flutter_application_1/main.dart';
-import 'package:flutter_application_1/screens/screens.dart';
+
 
 class AddRecipeScreen extends StatefulWidget {
   const AddRecipeScreen({super.key});
--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/models.dart';
-import 'package:flutter_application_1/main.dart';
+
 
 class RecipeDetailScreen extends StatelessWidget {
   final Recipe recipe;
